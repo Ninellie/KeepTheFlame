da5980133cacf06f269cba08aae366cc04f50697
Ninellie <a.pawlow2009@ya.ru>
2025-08-25 04:33:08 +0300

    [Add] Добавлена фича здоровья игрока, а также ассет конфига здоровья игрока

--- Assets/Scripts/GameLifetimeScope.cs
+++ Assets/Scripts/GameLifetimeScope.cs
@@ -1,5 +1,6 @@
 using Darkness;
 using Lamp;
+using PlayerHealth;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -8,6 +9,7 @@ public class GameLifetimeScope : LifetimeScope
 {
     [SerializeField] private LampFuelConfig lampFuelConfig;
     [SerializeField] private DarknessConfig darknessConfig;
+    [SerializeField] private PlayerHealthConfig healthConfig;
     
     protected override void Configure(IContainerBuilder builder)
     {
@@ -19,16 +21,24 @@ public class GameLifetimeScope : LifetimeScope
         
         var lampFuelGUI = GetLampFuelGUI(levelDebugGUIGameObject);
         builder.RegisterComponent(lampFuelGUI);
+        
         builder.RegisterEntryPoint<LampFuelBurner>();
         
         // Darkness
         builder.RegisterInstance(darknessConfig);
         builder.Register<DarknessPower>(Lifetime.Singleton);
-
+        
         var darknessPowerGUI = GetDarknessPowerGUI(levelDebugGUIGameObject);
         builder.RegisterComponent(darknessPowerGUI);
+        
         builder.RegisterEntryPoint<DarknessGatherer>();
 
+        // Health
+        builder.RegisterInstance(healthConfig);
+        builder.Register<PlayerHealth.PlayerHealth>(Lifetime.Singleton);
+        
+        var healthGUI = GetHealthGUI(levelDebugGUIGameObject);
+        builder.RegisterComponent(healthGUI);
     }
 
     private static GameObject GetLevelDebugGUIGameObject()
@@ -36,15 +46,18 @@ public class GameLifetimeScope : LifetimeScope
         return new GameObject("[LevelDebugGUI]");
     }
     
+    private static DebugPlayerHealthGUI GetHealthGUI(GameObject levelDebugGUIGameObject)
+    {
+        return levelDebugGUIGameObject.AddComponent<DebugPlayerHealthGUI>();
+    }
+    
     private static DebugLampFuelGUI GetLampFuelGUI(GameObject levelDebugGUIGameObject)
     {
-        var lampFuelGUI = levelDebugGUIGameObject.AddComponent<DebugLampFuelGUI>();
-        return lampFuelGUI;
+        return levelDebugGUIGameObject.AddComponent<DebugLampFuelGUI>();
     }
     
     private static DebugDarknessPowerGUI GetDarknessPowerGUI(GameObject levelDebugGUIGameObject)
     {
-        var darknessPowerGUI = levelDebugGUIGameObject.AddComponent<DebugDarknessPowerGUI>();
-        return darknessPowerGUI;
+        return levelDebugGUIGameObject.AddComponent<DebugDarknessPowerGUI>();
     }
 }
