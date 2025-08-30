ac4fade28a6c817d4874cad982c55c31a20d12ec
Ninellie <a.pawlow2009@ya.ru>
2025-08-30 21:29:04 +0300

    [Refactor] Убран закомментированный код в GameLifetimeScope.cs

--- Assets/Scripts/GameLifetimeScope.cs
+++ Assets/Scripts/GameLifetimeScope.cs
@@ -37,27 +37,23 @@ public class GameLifetimeScope : LifetimeScope
         builder.Register<FireflyPool>(Lifetime.Singleton);
         builder.Register<SpawnTimer>(Lifetime.Singleton);
         builder.Register<IDebugGUIWindow, DebugFireflySpawnerGUI>(Lifetime.Singleton);
-            // .Keyed(nameof(DebugFireflySpawnerGUI));
         
         // Lamp
         builder.RegisterInstance(lampFuelConfig);
         builder.RegisterEntryPoint<LampFuelBurner>();
         builder.Register<LampFuelTank>(Lifetime.Singleton);
         builder.Register<IDebugGUIWindow, DebugLampFuelGUI>(Lifetime.Singleton);
-            // .Keyed(nameof(DebugLampFuelGUI));
         
         // Darkness
         builder.RegisterInstance(darknessConfig);
         builder.RegisterEntryPoint<DarknessGatherer>();
         builder.Register<DarknessPower>(Lifetime.Singleton);
         builder.Register<IDebugGUIWindow, DebugDarknessPowerGUI>(Lifetime.Singleton);
-            // .Keyed(nameof(DebugDarknessPowerGUI));
 
         // Health
         builder.RegisterInstance(healthConfig);
         builder.Register<PlayerHealthCounter>(Lifetime.Singleton);
         builder.Register<IDebugGUIWindow, DebugPlayerHealthGUI>(Lifetime.Singleton);
-            // .Keyed(nameof(DebugPlayerHealthGUI));
         
         // Movement
         builder.RegisterInstance(playerMovementConfig);
@@ -74,13 +70,11 @@ public class GameLifetimeScope : LifetimeScope
         builder.Register<LampFlamePower>(Lifetime.Singleton);
         builder.RegisterEntryPoint<LampFlamer>();
         builder.Register<IDebugGUIWindow, DebugLampFlameGUI>(Lifetime.Singleton);
-            // .Keyed(nameof(DebugLampFlameGUI));
         
         // Darkness Damage
         builder.RegisterInstance(darknessDamageConfig);
         builder.RegisterEntryPoint<DarknessDamageDealer>();
         builder.Register<DebugDarknessDamageGUI>(Lifetime.Singleton).AsImplementedInterfaces()
             .AsSelf();
-            // .Keyed(nameof(DebugDarknessDamageGUI));
     }
 }
