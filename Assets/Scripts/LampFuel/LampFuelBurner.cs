2787f04f5088ab5c0d362553daac4ba2753c63a7
Ninellie <a.pawlow2009@ya.ru>
2025-08-30 20:33:28 +0300

    [Fix] Теперь топливо лампы не тратится, если лампа не горит.

--- Assets/Scripts/LampFuel/LampFuelBurner.cs
+++ Assets/Scripts/LampFuel/LampFuelBurner.cs
@@ -1,4 +1,5 @@
-﻿using VContainer.Unity;
+﻿using LampFlame;
+using VContainer.Unity;
 
 namespace LampFuel
 {
@@ -8,17 +9,20 @@ namespace LampFuel
         
         private readonly LampFuelTank _fuelTank;
         private readonly LampFuelConfig _config;
+        private readonly LampFlamePower _flame;
         
-        public LampFuelBurner(LampFuelTank fuelTank, LampFuelConfig config)
+        public LampFuelBurner(LampFuelTank fuelTank, LampFuelConfig config, LampFlamePower flame)
         {
             _fuelTank = fuelTank;
             _config = config;
-            
+            _flame = flame;
+
             Init();
         }
 
         public void FixedTick()
         {
+            if (!_flame.IsLit) return;
             if (_fuelTank.Value <= _fuelTank.Min) return;
             if (_config.decayPerSecond <= 0f) return;
             _fuelTank.Subtract(_config.decayPerSecond * UnityEngine.Time.deltaTime);
