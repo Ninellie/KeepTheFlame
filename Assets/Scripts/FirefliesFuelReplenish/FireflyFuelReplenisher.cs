636014473b05c6889bbc0b390de98c240b0de859
Ninellie <a.pawlow2009@ya.ru>
2025-08-30 22:04:21 +0300

    [Add] Добавлена механика пополнения топлива при подъёме светлячков

---
+++ Assets/Scripts/FirefliesFuelReplenish/FireflyFuelReplenisher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FirefliesSpawn;
+using LampFuel;
+
+namespace FirefliesFuelReplenish
+{
+    public class FireflyFuelReplenisher : IDisposable
+    {
+        public event Action OnFireflyReplenish; 
+        
+        private FireflyPool _pool;
+        private LampFuelTank _fuelTank;
+
+        public FireflyFuelReplenisher(FireflyPool pool, LampFuelTank fuelTank)
+        {
+            _pool = pool;
+            _fuelTank = fuelTank;
+            
+            _pool.OnFireflyCollected += Replenish; 
+        }
+
+        public void Replenish(Firefly firefly)
+        {
+            _fuelTank.Add(1);
+            OnFireflyReplenish?.Invoke();
+        }
+        
+        public void Dispose()
+        {
+            _pool.OnFireflyCollected -= Replenish;
+        }
+    }
+}
