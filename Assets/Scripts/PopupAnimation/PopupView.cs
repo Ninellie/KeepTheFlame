eda587208294bb21c88274068b1ea3c6b2354786
Ninellie <a.pawlow2009@ya.ru>
2025-09-04 00:45:02 +0300

    [Add] PopupView.cs теперь также работает с прозрачностью текста во время появления и скрытия

--- Assets/Scripts/PopupAnimation/PopupView.cs
+++ Assets/Scripts/PopupAnimation/PopupView.cs
@@ -1,4 +1,5 @@
 ﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace PopupAnimation
@@ -9,6 +10,7 @@ namespace PopupAnimation
     public class PopupView : MonoBehaviour
     {
         [SerializeField] private GameObject popup;
+        [SerializeField] private TMP_Text text;
         [SerializeField] private Transform animationStartPosition;
         [SerializeField] private Transform animationEndPosition;
         [SerializeField] private float openingDuration = 0.75f;
@@ -17,6 +19,7 @@ namespace PopupAnimation
         private void Start()
         {
             popup.SetActive(false);
+            text.color = new Color(1f, 1f, 1f, 0f);
             popup.transform.localPosition = animationStartPosition.localPosition; 
         }
         
@@ -24,12 +27,14 @@ namespace PopupAnimation
         {
             if (!other.CompareTag("Player")) return;
             popup.SetActive(true);
+            text.DOFade(1, openingDuration);
             popup.transform.DOLocalMoveY(animationEndPosition.localPosition.y, openingDuration);
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
             if (!other.CompareTag("Player")) return;
+            text.DOFade(0, closingDuration);
             popup.transform.DOLocalMoveY(animationStartPosition.localPosition.y, closingDuration)
                 .OnComplete( () => popup.SetActive(false));
         }
