cd69c85ff1f023488c5135d65e7c42468247099f
Ninel <neon3142@gmail.com>
2025-11-10 18:08:37 +0300

    [Fix] Исправлена ошибка. Добавлена отписка от событий в классе DarknessDamageDealer.cs

--- Assets/Scripts/DarknessDamage/DarknessDamageDealer.cs
+++ Assets/Scripts/DarknessDamage/DarknessDamageDealer.cs
@@ -1,12 +1,12 @@
-﻿using LampFlame;
+﻿using System;
+using LampFlame;
 using PlayerHealth;
 using UnityEngine;
-using VContainer;
 using VContainer.Unity;
 
 namespace DarknessDamage
 {
-    public class DarknessDamageDealer : IFixedTickable
+    public class DarknessDamageDealer : IFixedTickable, IDisposable
     {
         public int DamageAmount { get; private set; }
         public float DamageDealInterval { get; private set; }
@@ -17,6 +17,9 @@ namespace DarknessDamage
         private readonly DarknessDamageConfig _config;
         private readonly LampFlamePower _flame;
         private readonly PlayerHealthCounter _playerHealthCounter;
+        
+        private Action _onExtinguishedHandler;
+        private Action _onLitHandler;
         
         public DarknessDamageDealer(DarknessDamageConfig config, LampFlamePower flame, PlayerHealthCounter playerHealthCounter,
         DebugDarknessDamageGUI debugDarknessDamageGUI)
@@ -55,16 +58,28 @@ namespace DarknessDamage
         
         private void Subscribe()
         {
-            _flame.OnExtinguished += () =>
+            _onExtinguishedHandler = () =>
             {
                 _isFlameBurning = false;
                 SecondsToNextDamage = DamageDealInterval;
-            }; 
+            };
             
-            _flame.OnLit += () => { 
+            _onLitHandler = () => { 
                 _isFlameBurning = true;
                 SecondsToNextDamage = 0;
-            }; 
+            };
+            
+            _flame.OnExtinguished += _onExtinguishedHandler;
+            _flame.OnLit += _onLitHandler;
+        }
+        
+        public void Dispose()
+        {
+            if (_flame != null)
+            {
+                _flame.OnExtinguished -= _onExtinguishedHandler;
+                _flame.OnLit -= _onLitHandler;
+            }
         }
     }
 }
